5488a93175115b75a74606587e455e45a01e5d37
PeterNatig <peternatig@gmail.com>
2026-04-27 17:28:41 +0300

    Skill, UserSkills, TaskRequiredSkills Entites

--- TaskPilot.Domain/Entities/TaskRequiredSkill.cs
+++ TaskPilot.Domain/Entities/TaskRequiredSkill.cs
@@ -1,6 +1,14 @@
-﻿namespace TaskPilot.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskPilot.Domain.Entities
 {
     public class TaskRequiredSkill
     {
+        [ForeignKey("Task")]
+        public Guid TaskId { get; set; }
+        [ForeignKey("Skill")]
+        public int SkillId { get; set; }
+        public Task Task { get; set; }
+        public Skill Skill { get; set; }
     }
 }
