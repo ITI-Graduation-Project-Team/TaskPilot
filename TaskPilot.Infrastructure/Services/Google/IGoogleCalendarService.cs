9a0de4cc210381739d0af5cdc35099d0c58d095c
Doha-Fawzy <dohasaeed2003@gmail.com>
2026-07-23 11:41:33 +0300

    initial commit

---
+++ TaskPilot.Infrastructure/Services/Google/IGoogleCalendarService.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskPilot.Infrastructure.Services.Google;
+
+public interface IGoogleCalendarService
+{
+ 
+     //recieve link which employee or manager will click to login to google and give us permission to access his calendar
+    string GetGoogleLoginUrl();
+
+    //take the reurned code from google and exchange it for the refresh token (and save it)
+    Task<bool> ExchangeCodeForTokenAsync(string code, Guid userId);
+}
